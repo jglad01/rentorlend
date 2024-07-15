c470bfe23cfd0f9c038ee001fd1b1a723d9499f2
jglad01 <jak.gladysz@gmail.com>
2024-07-15 22:29:42 +0200

    ROL-1: Better currency setting dropdown button

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -118,7 +118,23 @@
                 <div id="dropdown-wrapper" class="relative inline-block text-left">
                   <div>
                     <button type="button" class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" id="menu-button" aria-expanded="true" aria-haspopup="true">
-                      Currency
+                      @php
+                          $currency = session('currency', 'PLN');
+                      @endphp
+                      @switch($currency)
+                          @case('PLN')
+                              🇵🇱 PLN
+                              @break
+                          @case('USD')
+                              🇺🇸 USD
+                              @break
+                          @case('EUR')
+                              🇪🇺 EUR
+                              @break
+                          @default
+                              🇵🇱 PLN
+                              
+                      @endswitch
                       <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                         <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
                       </svg>
@@ -137,10 +153,24 @@
                   -->
                   <div id="dropdown" class="hidden absolute right-0 z-10 mt-2 w-auto origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
                     <div class="py-1" role="none">
-                      <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
-                      <a href="setcurrency/PLN" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-0">🇵🇱 PLN</a>
-                      <a href="setcurrency/USD" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-1">🇺🇸 USD</a>
-                      <a href="setcurrency/EUR" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-2">🇪🇺 EUR</a>
+                      @switch($currency)
+                          @case('PLN')
+                              <a href="setcurrency/USD" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-1">🇺🇸 USD</a>
+                              <a href="setcurrency/EUR" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-2">🇪🇺 EUR</a>  
+                              @break
+                          @case('USD')
+                              <a href="setcurrency/PLN" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-0">🇵🇱 PLN</a>
+                              <a href="setcurrency/EUR" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-2">🇪🇺 EUR</a>  
+                              @break
+                          @case('EUR')
+                              <a href="setcurrency/PLN" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-0">🇵🇱 PLN</a>
+                              <a href="setcurrency/USD" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-1">🇺🇸 USD</a>
+                              @break
+                          @default
+                              <a href="setcurrency/USD" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-1">🇺🇸 USD</a>
+                              <a href="setcurrency/EUR" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-2">🇪🇺 EUR</a>  
+                              
+                      @endswitch
                     </div>
                   </div>
                 </div>
